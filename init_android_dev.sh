eef28a66c19fb8794e974162f54628fa524d503f
Trevor Drake <trevd1234@gmail.com>
2013-09-07 13:10:41 +0100

    added distinct installation of gcc 4.4 4.6 and 4.7 : Contarty to what seems popular believe you don't need install a different fucking operating system just because you want to build older versions of android, you don't even need to chroot FFS : update-alternatives takes care of all that shit and has been doing so since 1995 - it's the future
    switch gcc versions after installation using
    
    update-alternatives --config gcc
    
    Add missing auto yes to archivers and linaro-tools installations
    
    TESTED ON the commit date using lubuntu 13.04 64bit

--- init_android_dev.sh
+++ init_android_dev.sh
@@ -10,8 +10,9 @@
 
 SH=$(readlink /bin/sh)
 BASH=$( which bash )
-
-
+STUDIO_BUILD="130.687321"
+ANDROID_STUDIO_BUNDLE_FILENAME="android-studio-bundle-$STUDIO_BUILD-linux.tgz"
+ANDROID_STUDIO_BUNDLE_PATH="/var/cache/$ANDROID_STUDIO_BUNDLE_FILENAME"
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
@@ -69,6 +70,11 @@ echo "Webupd8 respository added for official oracle java jdk version 6, 7 & 8"
 sudo add-apt-repository --remove --yes ppa:phablet-team/tools
 sudo add-apt-repository --yes ppa:phablet-team/tools
 echo "Ubuntu Touch Developer Preview Tools repository added"
+
+sudo add-apt-repository --remove --yes ppa:linaro-maintainers/tools
+sudo add-apt-repository --yes ppa:linaro-maintainers/tools
+echo "Linaro ATools repository added"
+
 # Update 
 echo "Running apt-get update"
 sudo apt-get --yes update
@@ -79,16 +85,41 @@ sudo pkill -9 dpkg
 # Install both jdk's you never know when you might need to build Pre Gingerbread
 echo -e "Installing Java\nInstalling sun-java5-jdk" 
 sudo apt-get --yes install sun-java5-jdk
-sleep 2
 echo "Installing sun-java6-jdk" 
 sudo apt-get --yes install sun-java6-jdk 
-sleep 2
 # Install java 7 from wepupd8
 echo "Auto Accepting Oracle License"
 sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections -v
-sleep 2
 echo "Installing oracle-java7-installer" 
 sudo apt-get --yes  install oracle-java7-installer
+# Install java 6 from wepupd8
+# oracle-java6-installer is a newer version of sun-java6-jdk
+# we "need" both version to maintain the ability to build all versions
+# of Android on a single OS. 
+echo "Installing oracle-java6-installer" 
+sudo apt-get --yes  install oracle-java6-installer
+
+# GCC - We install multiple versions of gcc so we can use the same Operating System Version
+# to build all versions of Android - gcc 4.4 is capable of building early version of Android
+# without having to apply ridiculous patches which are never going to be submitted upstream
+# to the Android Main Code base  
+# 
+# update-alternative ( aka debian-alternatives ) is a mechanism which always easy switching 
+# between different versions of the same file. In this case we have made g++ a slave to gcc 
+# This keeps the binaries in sync with each other.
+# 
+# To switch between versions use : sudo update-alternatives --config gcc
+#
+echo "Installing GCC 4.4"
+sudo apt-get --yes install gcc-4.4 gcc-4.4-multilib g++-4.4
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 20 --slave /usr/bin/g++ g++ /usr/bin/g++-4.4
+echo "Installing GCC 4.6"
+sudo apt-get --yes install gcc-4.6 gcc-4.6-multilib g++-4.6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+echo "Installing GCC 4.7"
+sudo apt-get --yes install gcc-4.7 gcc-4.7-multilib g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+
 
 # Baseline AOSP Tool List
 # Note: Again Ignoring any official documentation which suggests installing
@@ -97,8 +128,8 @@ sudo apt-get --yes  install oracle-java7-installer
 # than Android
 
 # Instead of i386 packages we get the multi-arch compatible 32* versions
-echo "Install Required 32bit libraries"
-sudo apt-get --yes install lib32ncurses5-dev lib32readline6-dev lib32z1-dev
+echo "Installing Required 32bit libraries"
+sudo apt-get --yes install lib32ncurses5-dev lib32readline6-dev lib32z1-dev libc6-dev-i386 lib32stdc++6 
 
 # The build-essential packages contains a list of packages essential for building
 # debian but not really! see the package information for further details
@@ -116,7 +147,7 @@ sudo apt-get --yes install lib32ncurses5-dev lib32readline6-dev lib32z1-dev
 echo "Installing Build Essential MetaPackage"
 sudo apt-get --yes install build-essential 
 
-echo "Installing Cross Compilers and Multilib Utils"
+echo "Installing Latest Cross Compilers and Multilib Utils"
 sudo apt-get --yes install libc6-dev g++-multilib mingw32 mingw-w64
 
 sudo apt-get --yes install git-core gnupg flex bison  gperf  \
@@ -136,8 +167,10 @@ schedtool ubuntu-dev-tools
 # lzop          - Required if you want to enable lzo compression when building a kernel
 #                 as part of a Cyanogenmod installation
 echo "Installing lzop lzma zip xz archive support"
-sudo apt-get install lzop zip xz-utils zlib1g-dev
+sudo apt-get --yes install lzop zip xz-utils zlib1g-dev p7zip-full
  
+ echo "Installing linaro-image-tools"
+sudo apt-get --yes  install linaro-image-tools 
 fi
 shift
 echo "Creating udev [ /etc/udev/rules.d/51-android.rules ] rules for known android devices"
@@ -236,3 +269,17 @@ if [ ! -f /usr/bin/repo ] ; then
         sudo sh -c "curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > /usr/bin/repo"
         sudo chmod 755 /usr/bin/repo
 fi
+
+#
+
+#http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20130522.zip
+#echo "Downloading Android Studio"
+#sudo mkdir -p /var/cache/android-studio -m 0755
+
+#if [ ! -f $ANDROID_STUDIO_BUNDLE_PATH ] ; then 
+#        sudo sh -c "curl http://dl.google.com/android/studio/$ANDROID_STUDIO_BUNDLE_FILENAME > $ANDROID_STUDIO_BUNDLE_PATH"
+#fi
+
+#sudo tar  --extract --verbose --gunzip --file $ANDROID_STUDIO_BUNDLE_PATH --directory /var/cache
+
+
